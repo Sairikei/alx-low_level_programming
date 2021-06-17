e10e33f4215c1fd3999ecd1e99b310253af21f2d
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-17 12:37:41 -0700

    Cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  *main - Entry point 
  *
- *return: Always 0(Success)
+ *return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
