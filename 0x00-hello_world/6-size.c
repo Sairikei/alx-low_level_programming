b37f9dc1e0f7bce3bb22eb11d29160eacbc4a992
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-17 13:43:46 -0700

    Cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,4 +6,16 @@
  *Return: Always 0 (Success)
  */
 
-int main()
+int main(){
+	int inttype
+	float floattype
+	long  int longtype
+	long long int longlongtype
+	char chartype
+        printf("size of char:", sizeof(chartype));
+	printf("size of int:", sizeof(intype));
+	printf("size of long int:", sizeof(longtype));
+	printf("size of long long int:", sizeof(longlongtype));
+	printf("size of float:", sizeof(floattype));
+	return (0);
+}
