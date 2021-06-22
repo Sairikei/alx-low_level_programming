130e53678c67bfcdde621dc1e94827a888bcc050
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-22 08:42:55 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *main - print holberton
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char a[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	unsigned int i;
+	for (i = 0; i < sizeof(a); i++)
+	{
+		_putchar(a[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
