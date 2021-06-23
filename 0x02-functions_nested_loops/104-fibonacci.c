67e3b539668de7f608a005a2795f9fefd3398ad0
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-23 11:57:07 -0700

    nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "holberton.h"
+
 /**
  *main - print the first 98 numbers
  *
@@ -7,25 +7,31 @@
  */
 int main(void)
 {
-	int i = 98;
-	long t1 = 0, t2 = 1;
-	unsigned long int nextTerm = t1 + t2;
-	
-	while (i > 0)
-	{
-	printf("%lu", nextTerm);
-	if (i != 1)
-	{
-		printf(", ");
-	}
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	t1 = t2;
-	t2 = nextTerm;
-	nextTerm = t1 + t2;
-	i--;
-
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+				printf(", ");
 	}
 	printf("\n");
-	
 	return (0);
 }
