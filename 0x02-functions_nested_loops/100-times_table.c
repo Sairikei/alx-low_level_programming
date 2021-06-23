87852527d809a83b3bd99d172a4f009786067777
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-23 10:35:32 -0700

    nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,33 +5,43 @@
  *    */
 void print_times_table(int n)
 {
-	int i, j, pro;
-	if (!(n > 15 || n < 0))
+	int x = 0, y, z;
+
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			for (j = 0; j <= n; j++)
+			z = x * y;
+			if (z > 99)
 			{
-				pro = i * j;
-				if (j == 0)
-				{
-					_putchar(48);
-					continue;
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
 				_putchar(',');
 				_putchar(' ');
-				if (pro >= 10)
-				{
-					_putchar(pro / 10 + 48);
-					_putchar(pro % 10 + 48);
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(pro + 48);
-				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
+		x++;
 	}
 }
