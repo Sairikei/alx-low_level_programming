1495046a0e51b86f88eed5b9eba538da68bd1efa
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-23 06:28:12 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *print_to_98 - pritns to 98
+ *@n: integer to be tested
+ *Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	printf("\n");
+}
