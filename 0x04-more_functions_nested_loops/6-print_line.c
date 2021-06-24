640c917afdd4bd58b0846c286fd0a8a02b839dfc
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-24 10:35:40 -0700

    nested functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *print_line - print a straight ;ine in the terminal
+ *@n: number to be checked
+ *Return: straight line
+ */
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
