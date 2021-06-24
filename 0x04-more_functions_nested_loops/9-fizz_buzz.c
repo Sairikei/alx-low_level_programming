6656fa1441af76b84e00f72aa7bd88d44dfd4fc1
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-24 12:13:01 -0700

    holberton

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,25 +5,15 @@
  */
 void main(void)
 {
-	int i;
-
-	i = 1;
-	while (i <= 100)
+	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
+		if (i % 3 == 0)
+			printf("%s ", "Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("%s ", "Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("%s ", "FizzBuzz");
 		else
-			printf("%d", i);
-
-		if (i != 100)
-			printf(" ");
-		i++;
+			printf("%d ", i);
 	}
-	printf("\n");
-
-	return (0);
 }
