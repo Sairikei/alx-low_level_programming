d55388b2efd1796ce0926a28ff062ef14e8b7432
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-24 09:40:37 -0700

    nested functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - print 0 to 9 except 2 and 4
+ *Return: none
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_puchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
