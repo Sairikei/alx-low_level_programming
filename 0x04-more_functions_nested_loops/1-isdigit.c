537494335579fedaf453500beeca27a6e290e33f
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-24 07:51:57 -0700

    nested functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_isgigit - checks if c is between 0 and 9
+ *@c is the integer to be checked
+ *Return: 1 if c is digit
+ *else return 0
+ */
+int _isdigit(int c)
+{
+	if (c > 0 && c < 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
