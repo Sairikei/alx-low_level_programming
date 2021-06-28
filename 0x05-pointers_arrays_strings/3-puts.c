b290a0aff4b0d605d1fc5689d83374becd211ae5
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-06-28 11:22:12 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_puts - print a string followed by a new line
+ *
+ *@str: line to be printed in stdout
+ *Return: string
+ */
+void _puts(char *str)
+{	
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
