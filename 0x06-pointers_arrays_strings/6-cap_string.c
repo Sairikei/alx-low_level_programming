1bdbeaab8829f698d19f3ac7079acc978f18ccf8
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-01 12:44:51 -0700

    fixit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,24 @@
 #include "holberton.h"
-/**
- *string_toupper - changes all lowercase letters of a string
- *to uppercase
- *@s: string to modify 
- *
- *Return: the resulting string
- */
-char *string_toupper(char *s)
-{
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+ *cap_string - capitalizes all words of a string.
+ *@s: pointer to input string.
+ *Return: Returns pointer to capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
 	}
 	return (s);
 }
