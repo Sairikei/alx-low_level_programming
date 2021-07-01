fb5c1eaa978d3f204833c78fa0c9d184ab42bf4a
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-01 08:48:24 -0700

    more arrays strigs

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)) && n >= i)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;	
+	while (src[j] != '\0' && j < n)
 	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
