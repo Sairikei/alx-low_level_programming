72f780f4a8ea3bf48339cb720cbae4d1dce995d3
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-01 07:00:06 -0700

    more arrays strigs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,16 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = j = 0;
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 		i++;
-	while ((*(dest + 1) = *(src + j)))
+	while (src[j] != '\0')
+		j++;
+
 	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
