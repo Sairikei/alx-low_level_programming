f3ed6b9fca577e53de0b4b9e6b3dab75d7acfec3
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-07 11:34:40 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
 
 /**
- *_puts_recursion - prints a string
- *@s: to be tested
- *Return: string and a new line
+ *_puts_recursion - Print a string
+ *@s: string
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
+		_putchar('\n');
 	else
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
 }
