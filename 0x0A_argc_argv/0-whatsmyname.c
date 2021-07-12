c25e0a73aca42c81b3416946529c5772c8275bd5
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-12 06:14:45 -0700

    argc

---
+++ 0x0A_argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - prints program name
+ *@argc: takes arguement count
+ *@argv: arguements passed
+ *Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argc = "%d\n", argc);
+	return (0);
+}
