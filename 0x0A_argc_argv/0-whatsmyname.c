64b16bfbfedbd5d617360a1dfc5459047843faba
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-12 06:19:54 -0700

    argc

--- 0x0A_argc_argv/0-whatsmyname.c
+++ 0x0A_argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  *@argv: arguements passed
  *Return: return 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar(argc = "%d\n", argc);
+	printf("%s\n", argv[0]);
 	return (0);
 }
