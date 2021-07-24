5399b9ab5301f30e184aeb188dff00b8ad974aa5
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-24 02:01:42 -0700

    function pointers fix

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *array_iterator - executes a function given as a parameter
+ *@size: size of array
+ *@array: array
+ *@action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		while (size != 0)
+		{
+			(*action)(*array);
+			size--;
+			array++;
+		}
+	}
+}
