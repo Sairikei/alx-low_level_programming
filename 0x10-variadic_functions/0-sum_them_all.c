d9e59f78ca72be95a83d193a3308d71ef4151457
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-07-24 03:10:06 -0700

    vardiac functions fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int i, sum = 0;
+	int sum = 0;
+	unsigned int i;
+	va_list valist;
 
-	va_start(list, n);
-	if (n != 0)
-		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
-		;
-	va_end(list);
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
