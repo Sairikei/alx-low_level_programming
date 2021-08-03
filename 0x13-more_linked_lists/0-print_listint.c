3e8f1346db45546a960c3f4f21efd36f3d9fa05c
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-08-03 03:44:36 -0700

    more linked lists

--- 0x13-more_linked_lists/0-print_listint.c
+++ 0x13-more_linked_lists/0-print_listint.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 /**
- *print_listint - prints all elements of a listint_t list
- *@h: pointer to the list_t list to print
- *Return: the number of nodes printed
- */
+* print_listint -  prints all the elements of a listint_t list
+* @h: pointer to the list_t list to print
+* Return: the number of nodes printed
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t s = 0;
